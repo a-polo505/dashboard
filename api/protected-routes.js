cbba113b6bf3d1444dfed4066f49d2578d34ce3f
Alina Polozhai <polozhai.alina@hotmail.com>
2024-04-15 22:58:39 +0300

    Fix dependencies

--- api/protected-routes.js
+++ api/protected-routes.js
@@ -3,17 +3,18 @@ import { checkIPRestrictions } from "../src/ipRestrictions.js";
 
 const router = express.Router();
 
-router.get('/api/protected-routes', (req, res) => {
-    const userIPAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log(userIPAddress)
-    const isAllowed = checkIPRestrictions(userIPAddress);
-    if (isAllowed) {
-        console.log("isAllowed")
-        res.send('Welcome! You have access to this route.');
-    } else {
-        console.log("Access denied")
-        res.status(403).send('Access denied.');
-    }
+router.get("/api/protected-routes", (req, res) => {
+  const userIPAddress =
+    req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  console.log(userIPAddress);
+  const isAllowed = checkIPRestrictions(userIPAddress);
+  if (isAllowed) {
+    console.log("isAllowed");
+    res.send("Welcome! You have access to this route.");
+  } else {
+    console.log("Access denied");
+    res.status(403).send("Access denied.");
+  }
 });
 
 export default router;

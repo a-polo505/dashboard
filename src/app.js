63da78d876aa74ec1e07c8ba3258e0075abf1574
Alina Polozhai <polozhai.alina@hotmail.com>
2024-05-12 14:38:56 +0300

    Delete default container

--- src/app.js
+++ src/app.js
@@ -9,7 +9,3 @@ document.addEventListener("DOMContentLoaded", fetchDataAndDisplay);
 const defaultContainer1 = createContainer("Small", defaultContainerContent);
 defaultContainer1.classList.add("green-color");
 document.getElementById("col-3").appendChild(defaultContainer1);
-
-const defaultContainer2 = createContainer("Small", defaultContainerContent);
-defaultContainer2.classList.add("blue-color");
-document.getElementById("col-2").appendChild(defaultContainer2);

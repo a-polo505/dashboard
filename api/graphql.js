e84d3a2c395e1f26f4ad91fb9e5c6aa2033bbd37
Alina Polozhai <polozhai.alina@hotmail.com>
2024-06-03 23:10:46 +0300

    Fix graphql queries method

--- api/graphql.js
+++ api/graphql.js
@@ -1,19 +1,61 @@
-import { ApolloServer, gql } from 'apollo-server-micro';
+import { ApolloServer, gql } from "apollo-server-micro";
+import { InMemoryLRUCache } from "apollo-server-caching";
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
 
 const typeDefs = gql`
+  type Currency {
+    hello1: String
+  }
+
+  type Currency {
+    hello2: String
+  }
+
   type Query {
-    hello: String
+    currencies: [Currency]
+    currenciesDiff: [CurrencyDiff]
   }
 `;
 
 const resolvers = {
+  //   Query: {
+  //     hello: () => 'Hello from Apollo GraphQL on Vercel!',
+  //   },
+
   Query: {
-    hello: () => 'Hello from Apollo GraphQL on Vercel!',
+    currencies: async () => {
+      const client = new MongoClient(uri);
+      try {
+        await client.connect();
+        const collection = client
+          .db("yourDatabaseName")
+          .collection("currencies");
+        return await collection.find({}).toArray();
+      } finally {
+        await client.close();
+      }
+    },
+    currenciesDiff: async () => {
+      const client = new MongoClient(uri);
+      try {
+        await client.connect();
+        const collection = client
+          .db("yourDatabaseName")
+          .collection("currenciesDiff");
+        return await collection.find({}).toArray();
+      } finally {
+        await client.close();
+      }
+    },
   },
 };
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const cache = new InMemoryLRUCache();
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers, cache });
 
 await apolloServer.start();
 
-export default apolloServer.createHandler({ path: '/api/graphql' });
+export default apolloServer.createHandler({ path: "/api/graphql" });

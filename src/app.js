57b03f55d711ad272dc5662766ee49274d0c2ce7
Alina Polozhai <polozhai.alina@hotmail.com>
2024-10-30 15:16:58 +0200

    Fix code style

--- src/app.js
+++ src/app.js
@@ -1,11 +1,4 @@
-import { createContainer } from "./utils/container.js";
 import { fetchDataAndDisplay } from "./utils/fetchUtils.js";
 import "./utils/widgetInitializer.js";
 
-// const defaultContainerContent = '<p class="default-text-style">Soon 🛠</p>';
-
 document.addEventListener("DOMContentLoaded", fetchDataAndDisplay);
-
-// const defaultContainer1 = createContainer("Small", defaultContainerContent);
-// defaultContainer1.classList.add("green-color");
-// document.getElementById("col-3").appendChild(defaultContainer1);
